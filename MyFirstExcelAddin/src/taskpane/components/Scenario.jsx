bb2e363426fd8030de366b0f9ff4b00a753a5cf9
Bhasker Kumar <smaranneducations@gmail.com>
2024-03-25 09:02:50 +0100

    added dialouge box working2

--- MyFirstExcelAddin/src/taskpane/components/Scenario.jsx
+++ MyFirstExcelAddin/src/taskpane/components/Scenario.jsx
@@ -1,47 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from "./Footer";
 import { Button, Tooltip } from "@fluentui/react-components";
 import { ArrowCircleDownRegular, ArrowCircleUpRegular } from "@fluentui/react-icons";
 import "./Scenario.css";
-import { defineScenarioTable } from '../clientLogic/Scenario/defineScenarioTable.js';
-import ConfirmationDialog from './generic/ConfirmationDialog'; // Adjust the import path as necessary
-import { checkTableInNonScenarioSheets } from '../clientLogic/commonFunctions.js';
-
+import ConfirmationDialog1 from './generic/ConfirmationDialog1'; // Adjust the import path as necessary
 
 const Scenario = () => {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const dialogResponse = useSelector((state) => state.dialog.response);
 
-    useEffect(() => {
-        if (dialogResponse) {
-            console.log("User response:", dialogResponse);
-            // Additional logic based on the response
-        }
-    }, [dialogResponse]);
-
-    const handleDownButtonClick = async () => {
-        try {
-            const columns = await defineScenarioTable("Scenario");
-            // Console.log('Columns:', columns); is commented out, but you can uncomment to use
-        } catch (error) {
-            // Console.error('Error:', error); is commented out, but you can uncomment to use
-        }
-    };
-
-    const handleUpButtonClick = () => {
-        setIsDialogOpen(true);
-    };
-
-    const handleDialogClose = () => {
-        setIsDialogOpen(false);
-    };
-
-    const handleConfirm = () => {
-        console.log('Uploading data...');
-        setIsDialogOpen(false);
-        // Additional logic for what happens after confirmation
+    const handleDownButtonClick = () => {
+        setIsDialogOpen(true); // Use setState to change visibility
     };
 
     return (
@@ -59,13 +28,13 @@ const Scenario = () => {
                 </div>
                 <div className="download-button">
                     <Tooltip content="Upload data back to database" relationship="label">
-                        <Button size="large" icon={<ArrowCircleUpRegular />} onClick={handleUpButtonClick} />
+                        <Button size="large" icon={<ArrowCircleUpRegular />} />
                     </Tooltip>
                 </div>
             </div>
             <Footer />
             
-            <ConfirmationDialog 
+            <ConfirmationDialog1 
                 message="Are you sure you want to upload data?" 
                 isOpen={isDialogOpen} 
                 onOpenChange={setIsDialogOpen} 
